b040ad22afe9cdcf0cebe552270daf57d141063a
Brad Buran <info@bradburan.com>
2022-02-08 15:10:55 -0800

    Scale Babyface input so that 1V = 1 as reported by ASIO driver
    ASIO drivers seem to scale the input, so we need to correct for this
    scaling when measuring non-audio signals.

--- ncrar_audio/babyface.py
+++ ncrar_audio/babyface.py
@@ -7,7 +7,7 @@ class Babyface(SoundDevice):
     def __init__(self, ip_address=None, send_port=7001, recv_port=9001):
         self.osc_client = OSCClient(ip_address, send_port, recv_port)
         name = 'ASIO Fireface USB'
-        super().__init__(name, name)
+        super().__init__(name, name, input_scale=0.3395)
 
 
 if __name__ == '__main__':
